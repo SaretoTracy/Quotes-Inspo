bea1fd6d8161a7077d70411da19279babc92b052
saretotracy <tracysareto@gmail.com>
2022-02-05 16:33:37 +0300

    add another variable to quote class of type boolean

--- src/app/quotes.spec.ts
+++ src/app/quotes.spec.ts
@@ -2,6 +2,6 @@ import { Quotes } from './quotes';
 
 describe('Quotes', () => {
   it('should create an instance', () => {
-    expect(new Quotes(0, "")).toBeTruthy();
+    expect(new Quotes(0, "","")).toBeTruthy();
   });
 });
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,9 +1,10 @@
 export class Quotes {
     id:number;
     quote:string;
-
-    constructor(id:number, quote:string){
+    showauthor:boolean
+    constructor(id:number, quote:string, author:string){
 this.id=id;
 this.quote=quote;
+this.showauthor=false
     }
 }
