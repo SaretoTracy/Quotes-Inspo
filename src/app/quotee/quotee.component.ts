038e6ba9a4df8f069c24cb6a61718258c0312ea9
saretotracy <tracysareto@gmail.com>
2022-02-05 17:54:39 +0300

    add toogle effect

--- src/app/quotee/quotee.component.ts
+++ src/app/quotee/quotee.component.ts
@@ -11,6 +11,11 @@ export class QuoteeComponent implements OnInit {
    new Quotes(2, "You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box", "Author:Duchess Megan"),
    new Quotes(3, "I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened it’s normal. That is what is going to happen to you in life. No one gets through unscathed. We are all going to have a few scratches on us. Please be kind to yourselves and stand up for yourself, please.", "Author:Taylor Swift")
   ];
+  //toogle details
+  toggleDetails(index:number){
+    this.quotes[index].showauthor = !this.quotes[index].showauthor;
+  }
+
 
   constructor() { }
 
