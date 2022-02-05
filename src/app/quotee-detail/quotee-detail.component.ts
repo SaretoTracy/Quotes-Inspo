a6b65d1c17b6d4bfff56f283cdf57aad164be125
saretotracy <tracysareto@gmail.com>
2022-02-05 17:17:44 +0300

    modification

--- src/app/quotee-detail/quotee-detail.component.ts
+++ src/app/quotee-detail/quotee-detail.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quotee-detail',
   templateUrl: './quotee-detail.component.html',
   styleUrls: ['./quotee-detail.component.css']
 })
 export class QuoteeDetailComponent implements OnInit {
+  //input binding
   @Input() quote: any;
   
 
