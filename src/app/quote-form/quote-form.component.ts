08c5c03524f82c6d34e320e30fc2eae81bb13b90
saretotracy <tracysareto@gmail.com>
2022-02-07 00:39:52 +0300

    make for new quote same format as quote class

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,7 @@
+import { Quote } from '@angular/compiler';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,7 +9,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
- 
+ newQuote= new Quotes(0,"", "",0, 0, new Date());
   constructor() { }
 
   ngOnInit(): void {
