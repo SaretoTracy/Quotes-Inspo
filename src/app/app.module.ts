8f842017247404401b4a35b3c87cad77fa1bdde9
saretotracy <tracysareto@gmail.com>
2022-02-07 01:50:13 +0300

    add ng model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { QuoteeComponent } from './quotee/quotee.component';
 import { QuoteeDetailComponent } from './quotee-detail/quotee-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,6 +1,7 @@
-import { Quote } from '@angular/compiler';
+
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
 import { Quotes } from '../quotes';
 
 @Component({
@@ -10,6 +11,16 @@ import { Quotes } from '../quotes';
 })
 export class QuoteFormComponent implements OnInit {
  newQuote= new Quotes(0,"", "",0, 0, new Date());
+ @Output() addQuote = new EventEmitter<any>();
+ 
+ submitQuote(form: NgForm, formValue: any) {
+   if (form.valid) {
+     this.addQuote.emit(formValue);
+     form.reset();
+   } else {
+     alert("Enter name and quote please");
+   }
+ }
   constructor() { }
 
   ngOnInit(): void {
