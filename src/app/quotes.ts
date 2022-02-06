6a341338c460c252e231ee436a60d7af357b4c23
saretotracy <tracysareto@gmail.com>
2022-02-06 12:14:09 +0300

    use pipes to add date

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,7 @@
 export class Quotes {
     showauthor: boolean;//enable toggle between showing and hiding a author description
 
-  constructor(public id: number , public quote: string,public author: string, public like:number, public dislike:number, ){
+  constructor(public id: number , public quote: string,public author: string, public like:number, public dislike:number,public submissionDate:Date ){
     this.showauthor=false;
   }
 }
