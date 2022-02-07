eb059090ecf833a62b9280f1b34194157bee0233
saretotracy <tracysareto@gmail.com>
2022-02-07 05:19:09 +0300

    form styling

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -10,7 +10,7 @@ import { Quotes } from '../quotes';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
- newQuote= new Quotes(0,"", "",0, 0, "");
+ newQuote= new Quotes(0,"", "",0, 0, "", new Date());
  @Output() addQuote = new EventEmitter<Quotes>();
 
   submitQuote(){
