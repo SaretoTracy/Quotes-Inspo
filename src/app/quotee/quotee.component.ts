363ea370fe4647d3cbddfb0e32682b2f771a83ec
saretotracy <tracysareto@gmail.com>
2022-02-07 11:35:31 +0300

    form modification

--- src/app/quotee/quotee.component.ts
+++ src/app/quotee/quotee.component.ts
@@ -7,9 +7,9 @@ import { Quotes } from '../quotes';
 })
 export class QuoteeComponent implements OnInit {
   quotes: Quotes[] = [
-   new Quotes(1, "When you have a dream, you’ve got to grab it and never let go","Carol Burnest", 0 ,0,"Tracy Sareto",new Date(2022,  6, 6)),
-   new Quotes(2, "You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box", "Duchess Megan",0, 0,"Tracy Sareto",new Date(2022,  6, 6, 12, 49, 10)),
-   new Quotes(3, "I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened it’s normal. That is what is going to happen to you in life. No one gets through unscathed. We are all going to have a few scratches on us. Please be kind to yourselves and stand up for yourself, please.", "Taylor Swift",0,0,"Tracy Sareto",new Date(2022,  6, 6, 12, 49, 10))
+   new Quotes(1, "When you have a dream, you’ve got to grab it and never let go","Carol Burnest", 20 ,8,"Tracy Sareto",new Date(2022,  6, 6)),
+   new Quotes(2, "You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box", "Duchess Megan",25, 6,"Tracy Sareto",new Date(2022,  6, 6, 12, 49, 10)),
+   new Quotes(3, "I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened it’s normal. That is what is going to happen to you in life. No one gets through unscathed. We are all going to have a few scratches on us. Please be kind to yourselves and stand up for yourself, please.", "Taylor Swift",30,4,"Tracy Sareto",new Date(2022,  6, 6, 12, 49, 10))
   ];
   addNewQuote(quotes: Quotes){
     let quotesLength = this.quotes.length;
