b724d68ea08730640b83d20577920ac057f32c83
saretotracy <tracysareto@gmail.com>
2022-02-07 11:18:05 +0300

    edit time-count pipe

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -12,11 +12,11 @@ export class TimeCountPipe implements PipeTransform {
 
     let secondsPassed = (now - value.getTime()) / 1000; //time passed in second
 
-    if (secondsPassed < 60) {
+    if (secondsPassed > 60) {
       lastSeen = `${Math.floor(secondsPassed)} seconds ago `; // last seen in seconds
-    } else if (secondsPassed < 3600) {
+    } else if (secondsPassed > 3600) {
       lastSeen = `${Math.floor(secondsPassed / 60)} minutes ago`; //last seen in minutes
-    } else if (secondsPassed < 86400) {
+    } else if (secondsPassed > 86400) {
       lastSeen = `${Math.floor(secondsPassed / 3600)} hours ago`; //last seen in hours
     } else lastSeen = `${Math.floor(secondsPassed / 86400)} days ago`; //last seen in days
 
