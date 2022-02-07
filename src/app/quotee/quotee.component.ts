8344b1f1bf93c3767f9c0ec9e8cf385dd12aeaf5
saretotracy <tracysareto@gmail.com>
2022-02-07 03:08:11 +0300

    form modifications

--- src/app/quotee/quotee.component.ts
+++ src/app/quotee/quotee.component.ts
@@ -7,14 +7,13 @@ import { Quotes } from '../quotes';
 })
 export class QuoteeComponent implements OnInit {
   quotes: Quotes[] = [
-   new Quotes(1, "When you have a dream, you’ve got to grab it and never let go","Carol Burnest", 0 ,0,new Date(2022,2,4)),
-   new Quotes(2, "You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box", "Duchess Megan",0, 0,new Date(2022,2,6)),
-   new Quotes(3, "I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened it’s normal. That is what is going to happen to you in life. No one gets through unscathed. We are all going to have a few scratches on us. Please be kind to yourselves and stand up for yourself, please.", "Taylor Swift",0,0,new Date(2022,2,8))
+   new Quotes(1, "When you have a dream, you’ve got to grab it and never let go","Carol Burnest", 0 ,0,"Tracy Sareto"),
+   new Quotes(2, "You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box", "Duchess Megan",0, 0,"Tracy Sareto"),
+   new Quotes(3, "I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened it’s normal. That is what is going to happen to you in life. No one gets through unscathed. We are all going to have a few scratches on us. Please be kind to yourselves and stand up for yourself, please.", "Taylor Swift",0,0,"Tracy Sareto")
   ];
   addNewQuote(quotes: Quotes){
     let quotesLength = this.quotes.length;
     quotes.id = quotesLength+1;
-    quotes.submissionDate = new Date(quotes.submissionDate)
     this.quotes.push(quotes)
   }
   //toogle details
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,7 @@
 export class Quotes {
     showauthor: boolean;//enable toggle between showing and hiding a author description
 
-  constructor(public id: number , public quote: string,public author: string, public like:number, public dislike:number,public submissionDate:Date ){
+  constructor(public id: number , public quote: string,public author: string, public like:number, public dislike:number,public submission:string ){
     this.showauthor=false;
   }
 }
